a16244390d2e1a45e9fbe871ab1d4aa91594a072
giovanni <giovanni@canteradigital.mx>
2016-09-01 00:31:10 -0500

    Nueva BD

--- BD/minutas_trabajo.sql
+++ BD/minutas_trabajo.sql
@@ -78,11 +78,11 @@ DROP TABLE IF EXISTS `area`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `area` (
-  `id_area` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
+  `id_area` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(150) DEFAULT NULL,
   `descripcion` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_area`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +91,7 @@ CREATE TABLE `area` (
 
 LOCK TABLES `area` WRITE;
 /*!40000 ALTER TABLE `area` DISABLE KEYS */;
+INSERT INTO `area` VALUES (1,'Ventas','La que vende'),(2,'Recursos Humanos','Recursos Humanos'),(3,'Compras','Compras'),(4,'Contabilidad','Contabilidad');
 /*!40000 ALTER TABLE `area` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,7 +108,7 @@ CREATE TABLE `menu` (
   `controlador` varchar(255) DEFAULT NULL,
   `icono` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_menu`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +117,7 @@ CREATE TABLE `menu` (
 
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` VALUES (1,'Usuarios','usuarios/lista_usuarios','fa fa-user'),(2,'Puestos','puestos','fa fa-briefcase'),(3,'Areas','area','fa fa-building'),(4,'Minutas','minutas','fa fa-file-text'),(5,'Actividades','actividades','fa fa-list');
+INSERT INTO `menu` VALUES (1,'Usuarios','usuarios/lista_usuarios','fa fa-user'),(4,'Minutas','','fa fa-file-text'),(6,'Configuraciones',NULL,'fa fa-cog');
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,10 +158,8 @@ CREATE TABLE `puestos` (
   `nombre` varchar(100) DEFAULT NULL,
   `descripcion` varchar(255) DEFAULT NULL,
   `id_area` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_puestos`),
-  KEY `area_idx` (`id_area`),
-  CONSTRAINT `area` FOREIGN KEY (`id_area`) REFERENCES `area` (`id_area`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_puestos`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,7 +168,36 @@ CREATE TABLE `puestos` (
 
 LOCK TABLES `puestos` WRITE;
 /*!40000 ALTER TABLE `puestos` DISABLE KEYS */;
+INSERT INTO `puestos` VALUES (3,'Vendedor','El que vende ',1),(5,'Coordinador','El que coordina Bien',1),(6,'Directivo','Encargado del área de ventas',1),(7,'Contador','Contador',4),(8,'Encargado','Encargado',2);
 /*!40000 ALTER TABLE `puestos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `submenu`
+--
+
+DROP TABLE IF EXISTS `submenu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `submenu` (
+  `id_submenu` int(11) NOT NULL AUTO_INCREMENT,
+  `menu` varchar(100) DEFAULT NULL,
+  `controlador` varchar(150) DEFAULT NULL,
+  `id_menu` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_submenu`),
+  KEY `id_menu_idx` (`id_menu`),
+  CONSTRAINT `id_menu` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `submenu`
+--
+
+LOCK TABLES `submenu` WRITE;
+/*!40000 ALTER TABLE `submenu` DISABLE KEYS */;
+INSERT INTO `submenu` VALUES (1,'Puestos','puestos/lista_puestos',6),(2,'Areas','areas/lista_areas',6),(3,'Lista de Minutas','minutas/lista_minutas',4),(4,'Nueva Minuta','minutas/nueva_minuta',4),(5,'Actividades','minutas_actividades',4);
+/*!40000 ALTER TABLE `submenu` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -187,10 +215,13 @@ CREATE TABLE `usuarios` (
   `password` varchar(45) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `puesto` int(11) DEFAULT NULL,
+  `area` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_usuario`),
   KEY `puesto_idx` (`puesto`),
+  KEY `area_idx` (`area`),
+  CONSTRAINT `area` FOREIGN KEY (`area`) REFERENCES `area` (`id_area`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `puesto` FOREIGN KEY (`puesto`) REFERENCES `puestos` (`id_puestos`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -199,7 +230,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'admin',NULL,NULL,'password','admin@admin.com',NULL);
+INSERT INTO `usuarios` VALUES (1,'admin',NULL,NULL,'password','admin@admin.com',6,1),(2,'Jaime Giovanni','Guerra','Granados','gioguerra','giovanni@minutas.com',3,1),(4,'Maria','Alvarado','Solis','mari','mari_solis@gmail.com',7,4);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -220,4 +251,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-08-31  1:25:08
+-- Dump completed on 2016-09-01  0:30:34
